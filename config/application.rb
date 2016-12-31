a702d570e505d9db755a62b54c31854357d7ccd0
Sam Vary <sam.vary@gmail.com>
2016-12-31 09:46:07 -0500

    lose pdf.js

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,14 @@ module PersonalSite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
     config.serve_static_files = true
+    
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
