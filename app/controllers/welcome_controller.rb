3d47db50880792520de7386f27e899dfbb79bb4f
Sam Vary <sam.vary@gmail.com>
2016-12-31 16:34:31 -0500

    messing around

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,4 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def cv
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,11 @@ module PersonalSite
 
     config.serve_static_files = true
     
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
   end
 end
